1b3ce83b3f2c88def1edaf0b05635bf767a0f1d7
meteorst <1214045596@qq.com>
2019-08-15 16:36:30 +0800

    first commit test

---
+++ src/main/Java/com/meteorst/web/GitTest.java
@@ -0,0 +1,27 @@
+package com.meteorst.web;
+
+/**
+ * @Author: songtao
+ * @Date: 2019/8/15 16:33
+ */
+public class GitTest {
+
+    private String gitName;
+    private String gitEmail;
+
+    public String getGitName() {
+        return gitName;
+    }
+
+    public void setGitName(String gitName) {
+        this.gitName = gitName;
+    }
+
+    public String getGitEmail() {
+        return gitEmail;
+    }
+
+    public void setGitEmail(String gitEmail) {
+        this.gitEmail = gitEmail;
+    }
+}
